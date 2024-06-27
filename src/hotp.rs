b643b86a55f1f1d8c81b8c6d84a86ba7fd1e7f2b
xrjr <95307161+xrjr@users.noreply.github.com>
2024-06-27 02:24:15 +0200

    update all function to accept generic hash interface

--- src/hotp.rs
+++ src/hotp.rs
@@ -1,13 +1,16 @@
-use crate::{hmac::sha1_digest_from_bytes, sha1};
+use crate::hmac::HMAC;
+use crate::hash::HashFn;
 
-pub fn hotp(key: &[u8], counter: u64, digits: u32) -> u32 {
-    let hs = sha1_digest_from_bytes(counter.to_be_bytes().as_slice(), key);
+pub fn hotp<const B: usize, const L: usize, H: HashFn<B, L>, F: Fn() -> H>(hash_new_fn: F, key: &[u8], counter: u64, digits: u32) -> u32 {
+    let mut counter_hasher = HMAC::new(hash_new_fn, key);
+    counter_hasher.update(counter.to_be_bytes().as_slice());
+    let hs = counter_hasher.finalize();
     let snum = dt(&hs);
     snum % 10u32.pow(digits)
 }
 
 // dynamic truncation of sha1 digest
-fn dt(hs: &[u8; sha1::OUTPUT_BYTE_LENGTH]) -> u32 {
+fn dt<const L: usize>(hs: &[u8; L]) -> u32 {
     let offset = (hs[19] & 0xf) as usize;
     ((hs[offset] & 0x7f) as u32) << 24
         | ((hs[offset + 1] & 0xff) as u32) << 16
@@ -19,6 +22,7 @@ fn dt(hs: &[u8; sha1::OUTPUT_BYTE_LENGTH]) -> u32 {
 mod tests {
     use super::{dt, hotp};
     use crate::{hex, hmac};
+    use crate::sha1::SHA1;
 
     #[test]
     fn test_hotp_hmac_sha1_rfc() {
@@ -66,7 +70,7 @@ mod tests {
         ];
 
         for i in 0..expected.len() {
-            assert!(hotp(secret.as_slice(), i as u64, 6) == expected[i])
+            assert!(hotp(SHA1::new, secret.as_slice(), i as u64, 6) == expected[i])
         }
     }
 }
--- src/totp.rs
+++ src/totp.rs
@@ -1,13 +1,15 @@
 use crate::hotp;
+use crate::hash::HashFn;
+use crate::sha1::SHA1;
 
 fn number_of_time_steps(t0: u64, current: u64, x: u64) -> u64 {
     (current - t0) / x
 }
 
-pub fn totp(unix_time: u64, t0: u64, x: u64, digits: u32, key: &[u8]) -> u32 {
-    hotp::hotp(key, number_of_time_steps(t0, unix_time, x), digits)
+pub fn totp<const B: usize, const L: usize, H: HashFn<B, L>, F: Fn() -> H>(hash_new_fn: F, unix_time: u64, t0: u64, x: u64, digits: u32, key: &[u8]) -> u32 {
+    hotp::hotp(hash_new_fn, key, number_of_time_steps(t0, unix_time, x), digits)
 }
 
 pub fn totp_default(unix_time: u64, key: &[u8]) -> u32 {
-    totp(unix_time, 0, 30, 6, key)
+    totp(SHA1::new, unix_time, 0, 30, 6, key)
 }
