6006b50bd938f62449705e1ad7c5ce3a97068594
xrjr <95307161+xrjr@users.noreply.github.com>
2024-06-27 02:02:03 +0200

    hmac is now truely generic

--- src/totp.rs
+++ src/totp.rs
@@ -1,15 +1,13 @@
-use std::time::SystemTime;
-
 use crate::hotp;
 
 fn number_of_time_steps(t0: u64, current: u64, x: u64) -> u64 {
     (current - t0) / x
 }
 
-fn totp(unix_time: u64, t0: u64, x: u64, digits: u32, key: &[u8]) -> u32 {
+pub fn totp(unix_time: u64, t0: u64, x: u64, digits: u32, key: &[u8]) -> u32 {
     hotp::hotp(key, number_of_time_steps(t0, unix_time, x), digits)
 }
 
-fn totp_default(unix_time: u64, key: &[u8]) -> u32 {
+pub fn totp_default(unix_time: u64, key: &[u8]) -> u32 {
     totp(unix_time, 0, 30, 6, key)
 }
